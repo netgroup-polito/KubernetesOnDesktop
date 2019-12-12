17338c419f859c3111ecf34868e64c3dd858a3e2
s41m0n <simonemagnani.96@gmail.com>
2019-12-12 10:47:17 +0100

    Modified images and directories according to encryption

---
+++ docker/enc/Firefox/entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Start Xvfb
+Xvfb :99 -ac -screen 0 1920x1200x24 -nolisten tcp &
+
+#Export display env variable
+export DISPLAY=:99
+
+#Start ssh server
+/usr/sbin/sshd -D &
+
+#Start firefox in background
+firefox --no-sandbox &
+
+#Start vnc server in background
+x11vnc -once -localhost &
+
+#Wait for the first background process to terminate
+wait -n
+
+#Kill all the remaining background child processes
+pkill -P $$
