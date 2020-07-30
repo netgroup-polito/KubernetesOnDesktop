ae3786edea79d25f8e36244ae145fa4dece44b5d
Riccardo Roccaro <riccardoroccaro@gmail.com>
2020-07-30 20:10:56 +0200

    del old vncserv - renamed new_vncserver vncserver

--- docker/new_vncserver/app_image/config/docker-entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -xe
-set -e
-
-# Start ssh
-service ssh start
-
-### Switching to vncuser
-su -p $USER --command "/opt/config/user-startup.sh"
--- docker/vncserver/build_vncserver.sh
+++ docker/vncserver/build_vncserver.sh
@@ -1,32 +1,71 @@
 #!/bin/bash
 
+#Consts
+SRC_LINE_1='RUN apt-get install -y $APPLICATION'
+ESC_SRC_LINE_1=$(printf '%s\n' "$SRC_LINE_1" | sed -e 's/[]\/$*.^[]/\\&/g')
+
+SRC_LINE_2='### APP_LINE_2'
+ESC_SRC_LINE_2=$(printf '%s\n' "$SRC_LINE_2" | sed -e 's/[]\/$*.^[]/\\&/g')
+
+SRC_LINE_3='### APP_LINE_3'
+ESC_SRC_LINE_3=$(printf '%s\n' "$SRC_LINE_3" | sed -e 's/[]\/$*.^[]/\\&/g')
+
+DST_LINE_1='RUN apt-get install -y wget && \'
+ESC_DST_LINE_1=$(printf '%s\n' "$DST_LINE_1" | sed -e 's/[\/&]/\\&/g')
+
+DST_LINE_3='apt-get purge wget -y'
+ESC_DST_LINE_3=$(printf '%s\n' "$DST_LINE_3" | sed -e 's/[\/&]/\\&/g')
+
+TEMPLATE='./app_image/Dockerfile.template'
+DOCKERFILE='./app_image/Dockerfile'
+
+V="v1.0"
+
 function on_sigint {
-	#Uncommenting placeholder and deleting application line
-	sed -i "${placeholder_line_number}s/.*/$placeholder/" Dockerfile
+	#Removing the dockerfile leaving the template
+    rm -f $DOCKERFILE
+
+    trap -- SIGINT EXIT
+
+    exit $1
 }
 
-placeholder="#APPLICATION_PLACEHOLDER"
-placeholder_line_number=`grep -w -n "$placeholder" Dockerfile | cut -d : -f -1`
+function main {
+    #Copy the template to the Dockerfile
+    cp $TEMPLATE $DOCKERFILE
 
-if [ "$2" = "-m" ]; then
+    #Set trap to clean when it exits
+    trap on_sigint SIGINT EXIT
 
-	#TODO: need also the shared libraries
-	
-	if [ "$1" = "firefox" ]; then
-		to_be_added="RUN wget -qO- https://ftp.mozilla.org/pub/firefox/releases/7.0/linux-i686/en-US/firefox-7.0.tar.bz2 | tar -xj --strip 1 -C /opt \&\& chmod u+x /opt/*.sh \&\& ln -s /opt/firefox /usr/bin/firefox"
-	elif [ $1 = "libreoffice" ]; then
-		to_be_added="RUN wget -qO- http://download.documentfoundation.org/libreoffice/stable/6.0.0/deb/x86_64/LibreOffice_6.0.0_Linux_x86-64_deb.tar.gz | tar -xj --strip 1 -C /opt \&\& cd /opt/LibreOffice_6.0.0.3_Linux_x86-64_deb/DEBS/ \&\& sudo dpkg -i *.deb"
-	else
-		echo "$1 unsupported application, exiting."
-		exit 1
-	fi
-else
-	to_be_added='RUN apt-get install -y $APPLICATION'
-fi
+    if [ "$2" = "-m" ]; then    
+        #Setting the DST_LINE_2 according to the specified application
+        if [ "$1" = "firefox" ]; then
+            DST_LINE_2='wget -qO- https://ftp.mozilla.org/pub/firefox/releases/7.0/linux-i686/en-US/firefox-7.0.tar.bz2 | tar -xj --strip 1 -C /opt && chmod u+x /opt/*.sh && \'
+            V="v1.0-m"
+        elif [ $1 = "libreoffice" ]; then
+            DST_LINE_2='wget -qO- http://download.documentfoundation.org/libreoffice/stable/6.0.0/deb/x86_64/LibreOffice_6.0.0_Linux_x86-64_deb.tar.gz | tar -xj --strip 1 -C /opt && cd /opt/LibreOffice_6.0.0.3_Linux_x86-64_deb/DEBS/ && sudo dpkg -i *.deb && \'
+            V="v1.0-m"
+        else
+            echo "$1 unsupported application, exiting."
+            exit 1
+        fi
 
-sed -i "${placeholder_line_number}s^.*^$to_be_added^" Dockerfile
+        ESC_DST_LINE_2=$(printf '%s\n' "$DST_LINE_2" | sed -e 's/[\/&]/\\&/g')
 
-trap on_sigint SIGINT EXIT
+        #Replacing the app installing lines
+        sed -i "s%$ESC_SRC_LINE_1%$ESC_DST_LINE_1%g" $DOCKERFILE
+        sed -i "s%$ESC_SRC_LINE_2%$ESC_DST_LINE_2%g" $DOCKERFILE
+        sed -i "s%$ESC_SRC_LINE_3%$ESC_DST_LINE_3%g" $DOCKERFILE
 
-# The first argument is the name of the application we want in the docker
-docker build --no-cache --build-arg APPLICATION=$1 -t s41m0n/$1-headless-vnc .
+    fi
+
+    # Building the base image
+    docker build -t riccardoroccaro/base-headless-vnc:v1.0 ./base_image
+        
+    # The first argument is the name of the application we want in the docker
+    docker build --build-arg APPLICATION=$1 -t riccardoroccaro/$1-headless-vnc:$V ./app_image
+
+    exit 0
+}
+
+main $@
